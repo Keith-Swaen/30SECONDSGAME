fffd6510961be9cbdb5ed7cb1ea26e690f3b5f67
Keith Swaen <ketihrandyswaen29@gmail.com>
2022-06-16 09:11:20 +0200

    Initial Commit

---
+++ 30SECgame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30SECgame
+//
+//  Created by Keith Swaen on 10/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
